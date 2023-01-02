a5aa1faee11b3af8a9a43b88b7e63747cea13c58
ritsuki-cs <game.ricky.x@gmail.com>
2023-01-03 02:01:09 +0900

    質問一覧表示機能の作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,12 @@
 class QuestionsController < ApplicationController
   # 質問一覧表示
   def index
+    @questions = Question.all
   end
 
   # 質問詳細ページ表示
   def show
+    @question = Question.find(params[:id])
   end
 
   # 質問の作成
@@ -14,6 +16,12 @@ class QuestionsController < ApplicationController
 
   # 質問の登録
   def create
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBへ保存
+    @question.save
+    # showへリダイレクト
+    redirect_to @question
   end
 
   # 質問の編集
@@ -27,4 +35,9 @@ class QuestionsController < ApplicationController
   # 質問の削除
   def destroy
   end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :name, :content)
+    end
 end
