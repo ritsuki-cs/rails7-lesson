6305a26823e272e7dbb58ef9877e47ebd6eec8e8
ritsuki-cs <game.ricky.x@gmail.com>
2023-01-03 01:39:36 +0900

    質問フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # get 'users/index'
-  # get 'users/index', to: 'users#index'
-  get 'users', to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'users', to: 'users#index'
+  resources :questions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_152235) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_163047) do
+  create_table "questions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.integer "age"
