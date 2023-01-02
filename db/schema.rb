611dddfda23127731d83a0f9d8ab3ad0f3c2f675
ritsuki-cs <game.ricky.x@gmail.com>
2023-01-03 02:34:08 +0900

    回答用データベースモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_163047) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_173106) do
+  create_table "answers", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
   create_table "questions", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "title"
@@ -26,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_163047) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
