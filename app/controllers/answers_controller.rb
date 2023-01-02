1b200dc7c8355ded2e3d900b8f157788953b557e
ritsuki-cs <game.ricky.x@gmail.com>
2023-01-03 02:47:29 +0900

    新規回答投稿機能の追加

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,17 @@
+class AnswersController < ApplicationController
+  # 回答の登録
+  def create
+    @question = Question.find(params[:question_id])
+    @question.answers.create(answer_params)
+    redirect_to question_path(@question)
+  end
+
+  # 回答の削除
+  def destroy
+  end
+
+  private
+    def answer_params
+      params.require(:answer).permit(:name, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   # get 'users', to: 'users#index'
   root "questions#index"
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
 end
